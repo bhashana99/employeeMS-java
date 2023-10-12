37ff9a32ff5f8c567e9c5f6e47935b1d023e7f2f
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-12 22:46:25 +0530

    Add employee frame design & header

--- src/employeeMS/AddEmployee.java
+++ src/employeeMS/AddEmployee.java
@@ -1,5 +1,26 @@
 package employeeMS;
 
-public class AddEmployee {
-    
+import javax.swing.*;
+import java.awt.*;
+
+public class AddEmployee extends JFrame {
+
+    AddEmployee(){
+        getContentPane().setBackground(new Color(163,255,188));
+
+        JLabel heading = new JLabel("Add Employee Details");
+        heading.setBounds(320,30,500,50);
+        heading.setFont(new Font("serif", Font.BOLD,25));
+        add(heading);
+
+        setSize(900,700);
+        setLayout(null);
+        setLocation(300,50);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+            new AddEmployee();
+    }
+
 }
