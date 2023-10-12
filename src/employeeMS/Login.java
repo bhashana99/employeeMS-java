eebed62aec98848e17784bebfda21a2562cb081d
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-12 18:33:40 +0530

    add password label

--- src/employeeMS/Login.java
+++ src/employeeMS/Login.java
@@ -5,10 +5,14 @@ import javax.swing.*;
 public class Login extends JFrame {
 
     Login(){
-
+//
         JLabel username = new JLabel("Username");
         username.setBounds(40,20,100,30);
         add(username);
+
+        JLabel password = new JLabel("Password");
+        password.setBounds(40,70,100,30);
+        add(password);
 
         setSize(600,300);
         setLocation(450,200);
