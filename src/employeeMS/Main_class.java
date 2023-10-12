5a153a6561bce2d5bfc4ad36bb00f228d8aa1233
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-12 22:00:22 +0530

    Main frame design

--- src/employeeMS/Main_class.java
+++ src/employeeMS/Main_class.java
@@ -5,7 +5,9 @@ import javax.swing.*;
 public class Main_class extends JFrame {
 
     Main_class(){
-        setSize(500,500);
+        setSize(1120,630);
+        setLocation(250,100);
+        setLayout(null);
         setVisible(true);
     }
 
