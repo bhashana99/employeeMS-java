b3c6956c3eb2694f45d2994ff5e601420560b296
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-12 18:38:00 +0530

    add username to text field

--- src/employeeMS/Login.java
+++ src/employeeMS/Login.java
@@ -4,11 +4,16 @@ import javax.swing.*;
 
 public class Login extends JFrame {
 
+    JTextField tusername;
     Login(){
 //
         JLabel username = new JLabel("Username");
         username.setBounds(40,20,100,30);
         add(username);
+
+        tusername = new JTextField();
+        tusername.setBounds(150,20,150,30);
+        add(tusername);
 
         JLabel password = new JLabel("Password");
         password.setBounds(40,70,100,30);
