24ccecfceefdf80c251e88ad772e05879f6e9282
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-12 18:00:43 +0530

    image set to frame & size

--- src/employeeMS/Splash.java
+++ src/employeeMS/Splash.java
@@ -1,12 +1,18 @@
 package employeeMS;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Splash extends JFrame {
 
     Splash(){
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/front.gif"));
+        Image i2 = i1.getImage().getScaledInstance(1170,650,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0,0,1170,650);
+        add(image);
 
 
         setSize(1170,650);
