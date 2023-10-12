5ddb639c7eb34cb1ba0b40b460a2cd9eea014da8
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-12 17:51:45 +0530

    Store image variable

--- src/employeeMS/Splash.java
+++ src/employeeMS/Splash.java
@@ -5,6 +5,10 @@ import javax.swing.*;
 public class Splash extends JFrame {
 
     Splash(){
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/front.gif"));
+
+
         setSize(1170,650);
         setLocation(200,50);
         setLayout(null);
