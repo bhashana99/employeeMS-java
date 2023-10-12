5d629a13a86c7b52d69a9aa76ebad07125e78fb3
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-12 22:02:38 +0530

    add background image

--- src/employeeMS/Main_class.java
+++ src/employeeMS/Main_class.java
@@ -1,10 +1,20 @@
 package employeeMS;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main_class extends JFrame {
 
     Main_class(){
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/home.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1120,630,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image2 = new JLabel(i3);
+        image2.setBounds(0,0,1120,630);
+        add(image2);
+
+
         setSize(1120,630);
         setLocation(250,100);
         setLayout(null);
