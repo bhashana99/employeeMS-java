088d2f263272737a02640cabc3ae9e87d4d25da9
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-12 18:28:39 +0530

    add username label

--- src/employeeMS/Login.java
+++ src/employeeMS/Login.java
@@ -5,6 +5,11 @@ import javax.swing.*;
 public class Login extends JFrame {
 
     Login(){
+
+        JLabel username = new JLabel("Username");
+        username.setBounds(40,20,100,30);
+        add(username);
+
         setSize(600,300);
         setLocation(450,200);
         setLayout(null);
