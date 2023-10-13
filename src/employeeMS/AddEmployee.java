5d8da05931d4dd6bdd94465640966b5ef4f8426c
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-13 09:38:04 +0530

    add other label field

--- src/employeeMS/AddEmployee.java
+++ src/employeeMS/AddEmployee.java
@@ -1,11 +1,15 @@
 package employeeMS;
 
+import com.toedter.calendar.JCalendar;
+import com.toedter.calendar.JDateChooser;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class AddEmployee extends JFrame {
 
-    JTextField tname,tfname,taddress,tphone,tk,temail,tsalary,tdesignation;
+    JTextField tname,taddress,tphone,temail,tsalary;
+
     AddEmployee(){
         getContentPane().setBackground(new Color(163,255,188));
 
@@ -16,13 +20,58 @@ public class AddEmployee extends JFrame {
 
         JLabel name = new JLabel("Name");
         name.setBounds(50,150,150,30);
-        name.setFont(new Font("SAN_SERIF",Font.BOLD,25));
+        name.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         add(name);
 
         tname = new JTextField();
         tname.setBounds(200,150,150,30);
-        name.setBackground(new Color(177,252,197));
+        tname.setBackground(new Color(177,252,197));
         add(tname);
+
+        JLabel email = new JLabel("E-Mail");
+        email.setBounds(50,190,150,30);
+        email.setFont(new Font("SAN_SERIF",Font.BOLD,20));
+        add(email);
+
+        temail = new JTextField();
+        temail.setBounds(200,190,150,30);
+        temail.setBackground(new Color(177,252,197));
+        add(temail);
+
+        JLabel address = new JLabel("Address");
+        address.setBounds(50,230,150,30);
+        address.setFont(new Font("SAN_SERIF",Font.BOLD,20));
+        add(address);
+
+        taddress = new JTextField();
+        taddress.setBounds(200,230,150,30);
+        taddress.setBackground(new Color(177,252,197));
+        add(taddress);
+
+        JLabel phone = new JLabel("Phone Number");
+        phone.setBounds(50,270,150,30);
+        phone.setFont(new Font("SAN_SERIF",Font.BOLD,20));
+        add(phone);
+
+        tphone = new JTextField();
+        tphone.setBounds(200,270,150,30);
+        tphone.setBackground(new Color(177,252,197));
+        add(tphone);
+
+        JLabel salary = new JLabel("Salary");
+        salary.setBounds(50,310,150,30);
+        salary.setFont(new Font("SAN_SERIF",Font.BOLD,20));
+        add(salary);
+
+        tsalary = new JTextField();
+        tsalary.setBounds(200,310,150,30);
+        tsalary.setBackground(new Color(177,252,197));
+        add(tsalary);
+        
+
+
+
+
 
 
         setSize(900,700);
