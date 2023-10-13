3bf4936dc745e118cb988df671f04a75a91c98d3
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-13 13:42:43 +0530

    change field color and background color

--- src/employeeMS/AddEmployee.java
+++ src/employeeMS/AddEmployee.java
@@ -19,7 +19,7 @@ public class AddEmployee extends JFrame {
 
 
     AddEmployee(){
-        getContentPane().setBackground(new Color(163,255,188));
+        getContentPane().setBackground( Color.LIGHT_GRAY);
 
         JLabel heading = new JLabel("Add Employee Details");
         heading.setBounds(320,30,500,50);
@@ -33,7 +33,7 @@ public class AddEmployee extends JFrame {
 
         tname = new JTextField();
         tname.setBounds(200,150,150,30);
-        tname.setBackground(new Color(177,252,197));
+        tname.setBackground(new Color(240, 240, 240));
         add(tname);
 
         JLabel email = new JLabel("E-Mail");
@@ -43,7 +43,7 @@ public class AddEmployee extends JFrame {
 
         temail = new JTextField();
         temail.setBounds(200,190,150,30);
-        temail.setBackground(new Color(177,252,197));
+        temail.setBackground(new Color(240, 240, 240));
         add(temail);
 
         JLabel address = new JLabel("Address");
@@ -53,7 +53,7 @@ public class AddEmployee extends JFrame {
 
         taddress = new JTextField();
         taddress.setBounds(200,230,150,30);
-        taddress.setBackground(new Color(177,252,197));
+        taddress.setBackground(new Color(240, 240, 240));
         add(taddress);
 
         JLabel phone = new JLabel("Phone Number");
@@ -63,7 +63,7 @@ public class AddEmployee extends JFrame {
 
         tphone = new JTextField();
         tphone.setBounds(200,270,150,30);
-        tphone.setBackground(new Color(177,252,197));
+        tphone.setBackground(new Color(240, 240, 240));
         add(tphone);
 
         JLabel salary = new JLabel("Salary");
@@ -73,7 +73,7 @@ public class AddEmployee extends JFrame {
 
         tsalary = new JTextField();
         tsalary.setBounds(200,310,150,30);
-        tsalary.setBackground(new Color(177,252,197));
+        tsalary.setBackground(new Color(240, 240, 240));
         add(tsalary);
 
         JLabel dob = new JLabel("Date of Birth");
@@ -83,7 +83,7 @@ public class AddEmployee extends JFrame {
 
         tdob = new JDateChooser();
         tdob.setBounds(200,350,150,30);
-        tdob.setBackground(new Color(177,252,197));
+        tdob.setBackground(new Color(240, 240, 240));
         add(tdob);
 
         JLabel education = new JLabel("Education");
@@ -94,7 +94,7 @@ public class AddEmployee extends JFrame {
         String items[]={"PHD","MSC","MBA","BSc","B.Tech","B.COM"};
 
         Boxeducation = new JComboBox(items);
-        Boxeducation.setBackground(new Color(177,252,197));
+        Boxeducation.setBackground(new Color(240, 240, 240));
         Boxeducation.setBounds(200,390,150,30);
         add(Boxeducation);
 
@@ -105,7 +105,7 @@ public class AddEmployee extends JFrame {
 
         tnic = new JTextField();
         tnic.setBounds(200,430,150,30);
-        tnic.setBackground(new Color(177,252,197));
+        tnic.setBackground(new Color(240, 240, 240));
         add(tnic);
 
         JLabel empid = new JLabel("Employee Id");
