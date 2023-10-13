7544999f3d1f5033c91ca0e53087d97200984f5e
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-13 14:07:52 +0530

    change field position

--- src/employeeMS/AddEmployee.java
+++ src/employeeMS/AddEmployee.java
@@ -27,67 +27,67 @@ public class AddEmployee extends JFrame {
         add(heading);
 
         JLabel name = new JLabel("Name");
-        name.setBounds(50,150,150,30);
+        name.setBounds(250,120,150,30);
         name.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         add(name);
 
         tname = new JTextField();
-        tname.setBounds(200,150,150,30);
+        tname.setBounds(380,120,250,30);
         tname.setBackground(new Color(240, 240, 240));
         add(tname);
 
         JLabel email = new JLabel("E-Mail");
-        email.setBounds(50,190,150,30);
+        email.setBounds(250,160,150,30);
         email.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         add(email);
 
         temail = new JTextField();
-        temail.setBounds(200,190,150,30);
+        temail.setBounds(380,160,250,30);
         temail.setBackground(new Color(240, 240, 240));
         add(temail);
 
         JLabel address = new JLabel("Address");
-        address.setBounds(50,230,150,30);
+        address.setBounds(250,200,150,30);
         address.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         add(address);
 
         taddress = new JTextField();
-        taddress.setBounds(200,230,150,30);
+        taddress.setBounds(380,200,250,30);
         taddress.setBackground(new Color(240, 240, 240));
         add(taddress);
 
-        JLabel phone = new JLabel("Phone Number");
-        phone.setBounds(50,270,150,30);
+        JLabel phone = new JLabel("Telephone");
+        phone.setBounds(250,240,150,30);
         phone.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         add(phone);
 
         tphone = new JTextField();
-        tphone.setBounds(200,270,150,30);
+        tphone.setBounds(380,240,250,30);
         tphone.setBackground(new Color(240, 240, 240));
         add(tphone);
 
         JLabel salary = new JLabel("Salary");
-        salary.setBounds(50,310,150,30);
+        salary.setBounds(250,280,150,30);
         salary.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         add(salary);
 
         tsalary = new JTextField();
-        tsalary.setBounds(200,310,150,30);
+        tsalary.setBounds(380,280,250,30);
         tsalary.setBackground(new Color(240, 240, 240));
         add(tsalary);
 
         JLabel dob = new JLabel("Date of Birth");
-        dob.setBounds(50,350,150,30);
+        dob.setBounds(250,320,150,30);
         dob.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         add(dob);
 
         tdob = new JDateChooser();
-        tdob.setBounds(200,350,150,30);
+        tdob.setBounds(380,320,150,30);
         tdob.setBackground(new Color(240, 240, 240));
         add(tdob);
 
         JLabel education = new JLabel("Education");
-        education.setBounds(50,390,150,30);
+        education.setBounds(250,360,150,30);
         education.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         add(education);
 
@@ -95,26 +95,26 @@ public class AddEmployee extends JFrame {
 
         Boxeducation = new JComboBox(items);
         Boxeducation.setBackground(new Color(240, 240, 240));
-        Boxeducation.setBounds(200,390,150,30);
+        Boxeducation.setBounds(380,360,150,30);
         add(Boxeducation);
 
         JLabel nic = new JLabel("NIC");
-        nic.setBounds(50,430,150,30);
+        nic.setBounds(250,400,150,30);
         nic.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         add(nic);
 
         tnic = new JTextField();
-        tnic.setBounds(200,430,150,30);
+        tnic.setBounds(380,400,250,30);
         tnic.setBackground(new Color(240, 240, 240));
         add(tnic);
 
         JLabel empid = new JLabel("Employee Id");
-        empid.setBounds(50,470,150,30);
+        empid.setBounds(300,450,150,30);
         empid.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         add(empid);
 
-        tempid = new JLabel(""+number);
-        tempid.setBounds(200,470,150,30);
+        tempid = new JLabel("   "+number);
+        tempid.setBounds(420,450,150,30);
         tempid.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         tempid.setForeground(Color.red);
         add(tempid);
