27638150860ff552540b8be5d5dff4f4664203b5
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-13 08:47:48 +0530

    add name label and text feild

--- src/employeeMS/AddEmployee.java
+++ src/employeeMS/AddEmployee.java
@@ -5,6 +5,7 @@ import java.awt.*;
 
 public class AddEmployee extends JFrame {
 
+    JTextField tname,tfname,taddress,tphone,tk,temail,tsalary,tdesignation;
     AddEmployee(){
         getContentPane().setBackground(new Color(163,255,188));
 
@@ -12,6 +13,17 @@ public class AddEmployee extends JFrame {
         heading.setBounds(320,30,500,50);
         heading.setFont(new Font("serif", Font.BOLD,25));
         add(heading);
+
+        JLabel name = new JLabel("Name");
+        name.setBounds(50,150,150,30);
+        name.setFont(new Font("SAN_SERIF",Font.BOLD,25));
+        add(name);
+
+        tname = new JTextField();
+        tname.setBounds(200,150,150,30);
+        name.setBackground(new Color(177,252,197));
+        add(tname);
+
 
         setSize(900,700);
         setLayout(null);
