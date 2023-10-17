d9e3bf10f954f5f68c0518f38399e52f21282e09
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-17 22:29:00 +0530

    get all employees id

--- src/employeeMS/RemoveEmployee.java
+++ src/employeeMS/RemoveEmployee.java
@@ -2,15 +2,32 @@ package employeeMS;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.ResultSet;
 
 public class RemoveEmployee extends JFrame {
 
+    Choice choiceEmpId;
     RemoveEmployee(){
 
         JLabel label = new JLabel("Employee Id");
         label.setBounds(50,50,100,30);
         label.setFont(new Font("Tahoma",Font.BOLD,15));
         add(label);
+
+        choiceEmpId = new Choice();
+        choiceEmpId.setBounds(200,50,150,30);
+        add(choiceEmpId);
+
+        try {
+            conn c = new conn();
+            ResultSet resultSet = c.stmt.executeQuery("SELECT * FROM employee");
+            while (resultSet.next()){
+                choiceEmpId.add(resultSet.getString("empId"));
+                
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
 
         setSize(1000,400);
