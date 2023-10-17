156b14839b946b1d788188864d2d383a5434a16e
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-17 18:28:11 +0530

    extend jframe

--- src/employeeMS/ViewEmployee.java
+++ src/employeeMS/ViewEmployee.java
@@ -1,4 +1,15 @@
 package employeeMS;
 
-public class ViewEmployee {
+import javax.swing.*;
+
+public class ViewEmployee extends JFrame {
+
+    ViewEmployee(){
+
+        
+    }
+
+    public static void main(String[] args) {
+        new ViewEmployee();
+    }
 }
