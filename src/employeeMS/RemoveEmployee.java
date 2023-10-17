f673028eed2941e654798fc87f7bcff8b254032b
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-17 22:23:22 +0530

    add employee id label

--- src/employeeMS/RemoveEmployee.java
+++ src/employeeMS/RemoveEmployee.java
@@ -1,10 +1,16 @@
 package employeeMS;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class RemoveEmployee extends JFrame {
 
     RemoveEmployee(){
+
+        JLabel label = new JLabel("Employee Id");
+        label.setBounds(50,50,100,30);
+        label.setFont(new Font("Tahoma",Font.BOLD,15));
+        add(label);
 
 
         setSize(1000,400);
