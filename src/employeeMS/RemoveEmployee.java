e7fa49a7995ec20369f54c874697c88fa546ebc2
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-17 22:16:57 +0530

    remove employee class

---
+++ src/employeeMS/RemoveEmployee.java
@@ -0,0 +1,14 @@
+package employeeMS;
+
+import javax.swing.*;
+
+public class RemoveEmployee extends JFrame {
+
+    RemoveEmployee(){
+
+    }
+
+    public static void main(String[] args) {
+        new RemoveEmployee();
+    }
+}
