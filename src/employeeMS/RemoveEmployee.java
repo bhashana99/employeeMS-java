7bfcef16fbdbfa7eb6a30ae4fe1e688fbc1a8e3a
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-17 22:18:35 +0530

    remove employee frame

--- src/employeeMS/RemoveEmployee.java
+++ src/employeeMS/RemoveEmployee.java
@@ -6,6 +6,11 @@ public class RemoveEmployee extends JFrame {
 
     RemoveEmployee(){
 
+
+        setSize(1000,400);
+        setLocation(300,150);
+        setLayout(null);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
