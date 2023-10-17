d25209caff81805ade59f5bb74c6e85b2ed57319
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-17 18:40:00 +0530

    set background color

--- src/employeeMS/ViewEmployee.java
+++ src/employeeMS/ViewEmployee.java
@@ -1,10 +1,14 @@
 package employeeMS;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ViewEmployee extends JFrame {
 
     ViewEmployee(){
+
+        getContentPane().setBackground(new Color(50,120,200));
+
         setSize(900,700);
         setLayout(null);
         setLocation(300,100);
